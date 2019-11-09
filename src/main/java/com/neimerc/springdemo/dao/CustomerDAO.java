faa14bd6b524010a83977725cd1697956d6c0d5b
Neimerc <Neil_Mercury@yahoo.es>
2019-11-09 06:02:49 +0100

    Added DAO with CRUD operations

---
+++ src/main/java/com/neimerc/springdemo/dao/CustomerDAO.java
@@ -0,0 +1,16 @@
+package com.neimerc.springdemo.dao;
+
+import java.util.List;
+
+import com.neimerc.springdemo.entity.Customer;
+
+public interface CustomerDAO {
+	
+	public List<Customer> getCustomers();
+	
+	public void saveCustomer(Customer theCustomer);
+	
+	public Customer getCustomer(int theId);
+	
+	public void deleteCustomer(int theId);
+}

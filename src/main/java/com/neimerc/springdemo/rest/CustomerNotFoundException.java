5a5be5e0060be208656ad131f87b83151810ae4d
Neimerc <Neil_Mercury@yahoo.es>
2019-11-10 16:36:10 +0100

    Added serialVersionUID

--- src/main/java/com/neimerc/springdemo/rest/CustomerNotFoundException.java
+++ src/main/java/com/neimerc/springdemo/rest/CustomerNotFoundException.java
@@ -2,6 +2,8 @@ package com.neimerc.springdemo.rest;
 
 public class CustomerNotFoundException extends RuntimeException {
 
+	private static final long serialVersionUID = 1L;
+
 	public CustomerNotFoundException() {
 	}
 
